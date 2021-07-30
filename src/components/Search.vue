635815f2736a9cc33498dfd584bf05409b7b66e3
FernandoRub13 <pablo.rubio.bailon@gmail.com>
2021-07-30 10:55:59 -0500

    Subida al repositorio git

---
+++ src/components/Search.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="search-container">
+        
+        <input type="text" v-model="query" v-on:input="$emit('query-change', query)">
+        
+    </div>
+</template>
+
+<script>
+import { uuid } from 'vue-uuid';
+
+export default ({
+    name: 'Search',
+    data(){
+        return{
+            query: ''
+        }
+    },
+    methods:{
+        addTodo(e){
+            e.preventDefault();
+
+            const newTodo = {
+                id: uuid.v4(),
+                title: this.title,
+                completed: false
+            };
+
+            this.title = '',
+            this.$emit('add-todo', newTodo)
+        }
+    }
+
+})
+</script>
+
+<style scoped>
+    #search-container{
+        width: 600px;
+        margin: 0 auto;
+    }
+    input{
+        padding: 10px;
+        width: 100%;
+        outline: none;
+    }
+</style>
